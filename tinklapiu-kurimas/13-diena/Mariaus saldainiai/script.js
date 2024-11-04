a5a84e7b1797270672fa40f6da985a7465ffb84e
Arvydas Skripka <arvydas.skripka45@gmail.com>
2024-11-04 16:25:04 +0200

    Update script.js

--- tinklapiu-kurimas/13-diena/Mariaus saldainiai/script.js
+++ tinklapiu-kurimas/13-diena/Mariaus saldainiai/script.js
@@ -3,6 +3,6 @@ const Saldainiai = parseInt(prompt("Kiek saldainių gauna Marius?"))
 const kieksuvalge = parseInt(prompt("Po kiek saldainių suvalgo?"))
 const dienos = parseInt(prompt("Kelios dienos liko iki Kalėdų?"))
 
-const dovanos = (Saldainiai * dienos)
+const dovanos = Saldainiai * kieksuvalge
 
 console.log(`Marius dovanas paruoš ${dovanos} draugų`)
