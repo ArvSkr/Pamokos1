6b9a854acab4f10bc3eca5b0b151b08c9947a593
ArvSkr <arvydas.skripka45@gmail.com>
2024-11-05 11:56:22 +0200

    Add 14-diena

--- tinklapiu-kurimas/14-diena/Cikliniai Algoritmai. Nezinomo kartojimų skaičiaus ciklas/3. Saldainiai/script.js
+++ tinklapiu-kurimas/14-diena/Cikliniai Algoritmai. Nezinomo kartojimų skaičiaus ciklas/3. Saldainiai/script.js
@@ -1,1 +1,17 @@
-"use strict"
+"use strict";
+let gautiSaldainiai = parseInt(prompt("Petriukas gavo saldainiu:"));
+let likeSaldainiai = 0;
+let suvalgytiSaldainiai = 0;
+
+let dienos = 0;
+while (gautiSaldainiai > 0) {
+  suvalgytiSaldainiai = parseInt(prompt("Per diena suvalge:"));
+  gautiSaldainiai -= suvalgytiSaldainiai;
+  if (gautiSaldainiai > 0) {
+    likeSaldainiai = gautiSaldainiai;
+    dienos++;
+  }
+}
+console.log(
+  `Petriukui saldainiu uzteks ${dienos} dienoms ir jam liks ${likeSaldainiai} saldainiai`
+);
