3feb0f8d2e4c7e175de2c1e865bcb49a6238ba93
ArvSkr <arvydas.skripka45@gmail.com>
2024-11-05 12:41:44 +0200

    Add 14-diena

---
+++ tinklapiu-kurimas/14-diena/Cikliniai Algoritmai. Nezinomo kartojimų skaičiaus ciklas/1.Voveryte/script.js
@@ -0,0 +1,20 @@
+"use strict";
+let sukauptiRiesutai = parseInt(prompt(" Sukauptas riesutu kiekis X ="));
+let voveriukai = parseInt(prompt("Vovieriuku skaicius V="));
+
+
+let dienos = 0;
+while (sukauptiRiesutai > dienos) {
+  sukauptiRiesutai -= voveriukai * 3;
+  if (sukauptiRiesutai > 0) {
+    dienos++;
+  }
+
+}
+if (dienos > 92) {
+  console.log(`Riesutu sukaupta ${dienos} dienoms`);
+  console.log(`Riesutu uzteks visai ziemai`);
+} else {
+  console.log(`Riesutu sukaupta ${dienos} dienoms`);
+  console.log(`Riesutu neuzteks visai ziemai`);
+}
